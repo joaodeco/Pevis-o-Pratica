9fa801397faa1d36e3c9857df1389a2fa398068b
joaodeco <murilomergner@gmail.com>
2024-08-06 16:12:44 -0300

    Adicionei os ID`s

--- revisão.js
+++ revisão.js
@@ -5,9 +5,9 @@ const { editar } = require ('./editar')
 const { remover } = require ('./remover')
 
 let contatos = [
-    { nome: 'Alice', telefone: '1234-5678', email: 'alice@example.com' },
-    { nome: 'Bob', telefone: '8765-4321', email: 'bob@example.com' },
-    { nome: 'Carol', telefone: '5678-1234', email: 'carol@example.com' }
+    {id: 1, nome: 'Alice', telefone: '1234-5678', email: 'alice@example.com' },
+    {id: 2, nome: 'Bob', telefone: '8765-4321', email: 'bob@example.com' },
+    {id: 3, nome: 'Carol', telefone: '5678-1234', email: 'carol@example.com' }
   ];
 
 
