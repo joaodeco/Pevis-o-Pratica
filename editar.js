d39880770b81e77630f65a2ca643e2d7fa2f04cb
joaodeco <murilomergner@gmail.com>
2024-08-07 17:19:52 -0300

    Final

--- editar.js
+++ editar.js
@@ -3,28 +3,24 @@ const listar = require('./listar');
 const contatos = require('./contatos')
 
 function editar(contatos, callback){
-    if (contatos.length === 0){
-        console.log('Não a nenhum contato salvo neste monento, adicione um para poder editalos!');
-    } else {
-        listar();
-        let opcaoEditar = prompt('Qual desses você deseja editar')
-        const index = contatos.findIndex(contato => contato.id === opcaoEditar );
-        let nome = prompt('Digite o nome nome do contato!');
-        let telefones = []
-        let telefone
-        let email = prompt ('Agora para finalizar sua edição, o novo email!');
-        let jaExiste = contatos.find(cont => cont.email === email);
-        if (jaExiste){
-        console.log('Ja existe um contato com esse email!')
-    } else {
-        while((telefone = prompt('Qual seus telefones(se não tiver deixe em branco): '))){
-            telefones.push(telefone)
-            contatos.push(editar(index, {nome, telefone, email}));
-    }
-    
-    }
-     callback();
-    };
+    listar();
+    let opcaoEditar = prompt('Qual desses você deseja editar')
+    const index = contatos.findIndex(contato => contato.id === opcaoEditar );
+    let nome = prompt('Digite o nome nome do contato!');
+    let telefones = []
+    let telefone
+    let email = prompt ('Agora para finalizar sua edição, o novo email!');
+    let jaExiste = contatos.find(cont => cont.email === email);
+    if (jaExiste){
+    console.log('Ja existe um contato com esse email!')
+} else {
+    while((telefone = prompt('Qual seus telefones(se não tiver deixe em branco): '))){
+    telefones.push(telefone)
+        contatos.push(editar(index, {nome, telefone, email}));
+}
+
+}
+callback();
 };
 
 module.exports = editar
--- listar.js
+++ listar.js
@@ -1,20 +1,16 @@
-function listar() {
-    if (contatos.length == 0){
-        console.log('Não a nenhum contato salvo neste monento, adicione um para poder listalos!')
-    } else {
-      for(let i = 0; i < contatos.length; i++){
+function listar(contatos) {
+for(let i = 0; i < contatos.length; i++){
         const contato = contatos[i]
         console.log(`
         ID: ${contato.id},
         Nome: ${contato.nome},
         Email: ${contato.email},`)
-
-        for(let j = 0;j < contato.telefones.length; j++){
-          console.log(`   ${j+1}.  ${contatos.telefones[j]}`)
-        }
-        console.log('')
+      for(let j = 0;j < contato.telefones.length; j++){
+        console.log(`   ${j+1}.  ${contatos.telefones[j]}`)
       }
+      console.log('')
     }
-  };
+  }
+
 
 module.exports = listar;
